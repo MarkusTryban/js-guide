d2539c21cb3390331d76eab82aeea9ddde279494
MarkusTryban <markustryban@gmail.com>
2020-02-18 19:30:55 -0500

    Add more reverse string examples

--- string/string_reversal.js
+++ string/string_reversal.js
@@ -5,10 +5,36 @@ function reverseString(str) {
   // strArr.reverse();
   // return strArr.join('');
 
-  return str
-    .split('')
-    .reverse()
-    .join('');
+  // return str
+  //   .split('')
+  //   .reverse()
+  //   .join('');
+
+  // let revString = '';
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   revString = revString + str[i];
+  // }
+  // return revString;
+
+  // let revString = '';
+  // for (let i = 0; i <= str.length - 1; i++) {
+  //   revString = str[i] + revString;
+  // }
+  // return revString;
+
+  // let revString = '';
+  // for (let char of str) {
+  //   revString = char + revString;
+  // }
+  // return revString;
+
+  // let revString = '';
+  // str.split('').forEach(char =>
+  //   revString = char + revString);
+
+  // return revString;
+
+  return str.split('').reduce((revString, char) => char + revString, '');
 }
 
 const output = reverseString('hello');
