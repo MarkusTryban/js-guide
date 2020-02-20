a632f54487e1c1d94cce344fe193ba0735905b2f
MarkusTryban <markustryban@gmail.com>
2020-02-19 19:35:28 -0500

    Add reverse integer

---
+++ integer.js
@@ -0,0 +1,13 @@
+const reverseInt = int => {
+  const revString = int
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(revString) * Math.sign(int);
+};
+
+const output = reverseInt('12345');
+
+console.log(output);
