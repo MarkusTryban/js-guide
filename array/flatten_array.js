2ea42f34c39a32d8e15a46ff9a9776733f43150f
MarkusTryban <markustryban@gmail.com>
2020-02-24 20:55:44 -0500

    Update with new solutions

--- array/flatten_array.js
+++ array/flatten_array.js
@@ -1,7 +1,9 @@
 const flattenArray = arrays => {
-  return arrays.reduce((a, b) => {
-    return a.concat(b);
-  });
+  // return arrays.reduce((a, b) => a.concat(b));
+
+  // return [].concat.apply([], arrays);
+
+  return [].concat(...arrays);
 };
 
 const output = flattenArray([[1, 2], [3, 4], [5, 6], [7, 8], [9]]);
