a6427f6fe0ff5b157b83def1c0373c3094359055
MarkusTryban <markustryban@gmail.com>
2020-02-25 19:47:21 -0500

    Change every letter of the string

---
+++ string/letter_changes.js
@@ -0,0 +1,18 @@
+// Change every letter of the string to the one that follows it and capitalize the vowels
+function letterChanges(str) {
+  let newStr = str.toLowerCase().replace(/[a-z]/gi, char => {
+    if (char === 'z' || char === 'Z') {
+      return 'a';
+    } else {
+      return String.fromCharCode(char.charCodeAt() + 1);
+    }
+  });
+
+  newStr = newStr.replace(/a|e|i|o|u/gi, vowel => vowel.toUpperCase());
+
+  return newStr;
+}
+
+const output = letterChanges('Hello There');
+
+console.log(output);
