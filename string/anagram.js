28b38851e6e62354615d7a953a80966c5fda07e9
MarkusTryban <markustryban@gmail.com>
2020-02-25 19:39:57 -0500

    Return true if anagram and false if not

---
+++ string/anagram.js
@@ -0,0 +1,16 @@
+const isAnagram = (str1, str2) => {
+  return formatString(str1) === formatString(str2);
+};
+
+const formatString = str => {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+};
+
+const output = isAnagram('elbow', 'below'); // --> true
+
+console.log(output);
