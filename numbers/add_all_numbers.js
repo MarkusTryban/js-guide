399b2964d96f3723a434a3204853f6989002eb1e
MarkusTryban <markustryban@gmail.com>
2020-02-25 20:17:55 -0500

    Add new solution to add_all_numbers.js

--- numbers/add_all_numbers.js
+++ numbers/add_all_numbers.js
@@ -1,12 +1,22 @@
-function addAll() {
-  let args = Array.prototype.slice.call(arguments);
-  let total = 0;
+// function addAll() {
+//   let args = Array.prototype.slice.call(arguments);
+//   let total = 0;
 
-  for (let i = 0; i < args.length; i++) {
-    total += args[i];
-  }
+//   for (let i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
 
-  return total;
-}
+//   return total;
+// }
 
-console.log(addAll(2, 5, 6, 7, 9));
+// console.log(addAll(2, 5, 6, 7, 9));
+
+const addAllNum = (...numbers) => {
+  // let total = 0;
+  // numbers.forEach(num => (total += num));
+  // return total;
+
+  return numbers.reduce((acc, cur) => acc + cur);
+};
+
+console.log(addAllNum(2, 5, 6, 7, 9));
