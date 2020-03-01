ccdd517b59107cf9341cfc058884fdf94cab2a2b
MarkusTryban <markustryban@gmail.com>
2020-02-29 20:15:19 -0500

    Add new solution to seek_and_destroy

--- array/seek_and_destroy.js
+++ array/seek_and_destroy.js
@@ -1,11 +1,15 @@
-function seekAndDestroy(arr) {
-  const args = Array.from(arguments);
+// function seekAndDestroy(arr) {
+//   const args = Array.from(arguments);
 
-  function filterArr(arr) {
-    return args.indexOf(arr) === -1;
-  }
+//   function filterArr(arr) {
+//     return args.indexOf(arr) === -1;
+//   }
 
-  return arr.filter(filterArr);
-}
+//   return arr.filter(filterArr);
+// }
+
+const seekAndDestroy = (arr, ...rest) => {
+  return arr.filter(val => !rest.includes(val));
+};
 
 console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6));
