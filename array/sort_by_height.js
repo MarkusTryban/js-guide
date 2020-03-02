c51e570ed457a1a98f594563d5dca108173045da
MarkusTryban <markustryban@gmail.com>
2020-03-01 20:02:12 -0500

    Return array of sorted elements

---
+++ array/sort_by_height.js
@@ -0,0 +1,14 @@
+const sortByHeight = a => {
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => (val === -1 ? arr1.push(i) : arr2.push(val)));
+
+  const sortArr = arr2.sort((a, b) => a - b);
+
+  arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1));
+
+  return sortArr;
+};
+
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
